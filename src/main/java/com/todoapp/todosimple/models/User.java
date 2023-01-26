e1ff5d53b1354a2c77da499fc0dff20facd7af19
Heraldo <heraldoribeiro84@gmail.com>
2023-01-26 10:29:40 -0400

    Terceiro Commit

--- src/main/java/com/todoapp/todosimple/models/User.java
+++ src/main/java/com/todoapp/todosimple/models/User.java
@@ -1,15 +1,28 @@
 package com.todoapp.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Entity
 public class User {
+
+    public interface CreateUser {
+
+    }
+
+    public interface UpdateUser {
+
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,11 +30,25 @@ public class User {
     private Integer id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotEmpty
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
     private String username;
 
     @Column(name = "password", length = 60, nullable = false)
-    @NotEmpty
-    private String password;
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 8, max = 60)
+    private String password;    
+
+    //private List<Task> tasks = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }   
 
 }
